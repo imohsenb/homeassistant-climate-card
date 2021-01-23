82a5df660e3810916842bb95322d6b1d87a5f21e
Mohsen Beiranvand <imohsen.b@gmail.com>
2021-01-23 17:54:58 +0100

    Add images

--- src/climate/ClimateCardConfig.ts
+++
@@ -1,5 +0,0 @@
-import CardConfig from '../core/CardConfig'
-
-export default interface ClimateCardConfig extends CardConfig{
-    name?: string;
-}
--- src/core/CardConfig.ts
+++
@@ -1,5 +0,0 @@
-import { LovelaceCardConfig } from 'custom-card-helpers';
-
-export default interface CardConfig extends LovelaceCardConfig{
-    entity?: string;
-}
